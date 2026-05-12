95271f8f4a5c00027611d937c9730cec4376315c
shawonstack <shawon934764@gmail.com>
2026-05-12 12:44:29 +0600

    multiple information access using bactic string

--- day-1.js
+++ day-1.js
@@ -1,18 +1,39 @@
-// const numbers = [34, 23, 45, 7, 26, 8, 32, 424, 3, 23, 23, 523, 42, 42];
-// numbers.push(45);
-// console.log(numbers);
-let foods = [
-  'am',
-  'jam',
-  'kathal',
-  'kola',
-  'licu',
-  'peyara',
-  'bedona',
-  'malta',
-  'pinaple',
-  'orrange',
-];
-for (let food of foods) {
-  console.log(food);
-}
+// // const numcg(numbers);
+// let foods = [
+//   'am',
+//   'jam',
+//   'kathal',
+//   'kola',
+//   'licu',
+//   'peyara',
+//   'bedona',
+//   'malta',
+//   'pinaple',
+//   'orrange',
+// ];
+// for (let food of foods) {
+//   console.log(food);
+// }
+
+// let number = 45;
+// if (number % 2 === 0) {
+//   console.log('we g Even number');
+// } else {
+//   console.log('we go odd number');
+// }
+// const isStudent = true;
+// if (isStudent == true) {
+//   console.log('valo chele');
+// } else {
+//   console.log('kharp chele');
+// }
+
+let information = {
+  name: 'shawon',
+  age: 23,
+  english: 'b2',
+  study: 'Usa',
+};
+console.log(
+  `amr name holo ${information.name} amr age holo ${information.age} ami bortomane english ${information.english} level e achi. ami bortomn ${information.study} te cse niye proasuna kortesi`,
+);
