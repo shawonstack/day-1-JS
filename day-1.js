e376fe0e84e852cfbf1f506b413df2a330eb54a8
shawonstack <shawon934764@gmail.com>
2026-05-12 12:20:15 +0600

    condition sed

--- day-1.js
+++ day-1.js
@@ -1,2 +1,18 @@
-const name = 'shawon';
-console.log('hey may name is ', name);
+// const numbers = [34, 23, 45, 7, 26, 8, 32, 424, 3, 23, 23, 523, 42, 42];
+// numbers.push(45);
+// console.log(numbers);
+let foods = [
+  'am',
+  'jam',
+  'kathal',
+  'kola',
+  'licu',
+  'peyara',
+  'bedona',
+  'malta',
+  'pinaple',
+  'orrange',
+];
+for (let food of foods) {
+  console.log(food);
+}
